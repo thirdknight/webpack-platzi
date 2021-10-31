078d0338beb7cf980f3dcbc71e5318062eec2343
Edinson <edinson.ningles@gmail.com>
2021-11-01 00:01:56 +0100

    ~init

---
+++ README.md
@@ -0,0 +1,12 @@
+Create Folder src and File index.js
+
+npm init -y
+
+//Create webpack local(It will crate Folder "dist")
+npx webpack 
+npx webpack --mode development
+npx webpack --mode production
+
+
+// Tuto project
+npx webpack --mode production --config webpack.config.js
---
+++ webpack.config.js
@@ -0,0 +1,67 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js',
+    assetModuleFilename: 'assets/images/[hash][ext][query]'
+  },
+  resolve: {
+    extensions: ['.js']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.s?css$/,
+        use: [MiniCssExtractPlugin.loader,
+            "css-loader",
+            "sass-loader"]
+      },
+      {
+        test: /\.png/,
+        type: 'asset/resource'
+      },
+      {
+        test: /\.(woff|woff2)$/,
+        use: {
+          loader: 'url-loader',
+          options: {
+            limit: 10000,
+            minitype: "application/font-woff",
+            name: "[name]."[ext],
+            outputPath: "./assets/fonts/",
+            publicPath: "./assets/fonts",
+            esModule: false,
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: './public/index.html',
+      filename: './index.html'
+    }),
+    new MiniCssExtractPlugin(),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, "src", "assets/images"),
+          to: "assets/images"
+        }
+      ]
+    })
+  ]
+}
