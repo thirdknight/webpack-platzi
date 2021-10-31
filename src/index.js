1bd5a7c19dcfca0954882ac6c2630873353ebb12
Edinson <edinson.ningles@gmail.com>
2021-11-01 00:41:09 +0100

    webpack alias

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import Template from './templates/Template.js';
-import './styles/main.css';
-import './styles/vars.scss';
+import Template from '@templates/Template.js';
+import '@styles/main.css';
+import '@styles/vars.scss';
 
 (async function App() {
   const main = null || document.getElementById('main');
